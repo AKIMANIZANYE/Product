d024fc5c1bd50a6b441e9f2773e2630f238e4486
Claudine <you@example.com>
2020-01-31 17:56:32 +0200

    first commit

--- app.js
+++ app.js
@@ -28,11 +28,14 @@ let data = '';
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
     console.log(JSON.parse(data));
-  });
+    
 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
+ });
+    
+
+
 });
+
 
 
 
