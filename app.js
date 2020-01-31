f50e34be9130ad7817f52b38dc65fa09c471c34f
Claudine <you@example.com>
2020-01-31 16:59:47 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+const express = require('express');
+const app  =express();
+
+const http = require('http');
+const request = require('request');
+
+app.get('/request',()=> {   
+    console.log('request  is good');
+ 
+});
+
+request('http://dev.mobivat.com:8080/vsdc_module/mobivat/api/product/productId?upc=224444445', { json: true }, (err, res, body) => {
+  if (err)
+   { return console.log(err); }
+  console.log(body.url);
+
+});
+
+http.get('http://dev.mobivat.com:8080/vsdc_module/mobivat/api/product/productId?upc=224444445', (resp) => {
+  
+let data = '';
+  resp.on('data', (result) => {
+    data += result;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+
+
+
+
+
+app.listen(3000,() => {console.log('starting server');
+});
